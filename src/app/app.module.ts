5b5260473f577b3a81cf36b3f788450719df902f
lingling8498 <Ling.Wang@WolfDen.wolfmedical.com>
2018-06-14 18:58:36 -0700

    Added Dynamic Form - DynamicFormComponent and DynamicFormQuestionComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './config/config.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigComponent
+    ConfigComponent,
+    DynamicFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
     HttpClientXsrfModule
